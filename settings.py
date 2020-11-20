5c71ac9295a7ff4ee93ae1cf966ff470d4a0dc48
yangjiakang <18817958817@163.com>
2020-11-20 14:16:21 +0800

    第一次提交

---
+++ settings.py
@@ -0,0 +1,16 @@
+class Settings:
+    """存储游戏《外星人入侵》中所有设置的类"""
+
+    def __init__(self):
+        """初始化游戏的设置"""
+        # 设置屏幕
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
+        self.ship_speed = 1.5
+
+        # 子弹设置
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
