33017c80b2edef1da794ada7d224fa1114e3709b
yangjiakang <18817958817@163.com>
2020-11-30 15:15:32 +0800

    删除消失的子弹3

--- alien_invasion.py
+++ alien_invasion.py
@@ -88,8 +88,8 @@ class AlienInvasion:
     def _fire_bullet(self):
         """创建一颗子弹，并将其加入编组bullets中"""
         if len(self.bullets) < self.settings.bullets_allowed:
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
 
 
