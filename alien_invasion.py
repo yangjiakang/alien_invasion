41275eddc5c476ad2a56b429ac7c2d6e164e5eaa
yangjiakang <18817958817@163.com>
2020-11-30 15:25:05 +0800

    优化run_game()方法，创建_update_bullets()

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,13 +32,14 @@ class AlienInvasion:
             # 监视键盘和鼠标事件
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
+            # self.bullets.update()
 
             # 删除消失的子弹
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-            print(len(self.bullets))
+            # for bullet in self.bullets.copy():
+            #     if bullet.rect.bottom <= 0:
+            #         self.bullets.remove(bullet)
+            # print(len(self.bullets))
 
             # 每次循环时都重绘屏幕
             self._update_screen()
@@ -56,6 +57,17 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+
+    def _update_bullets(self):
+        """更新子弹的位置并删除消失的子弹"""
+        # 更新子弹的位置
+        self.bullets.update()
+        # 删除消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        print(len(self.bullets))
+
 
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
@@ -90,6 +102,7 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
 
 
 
