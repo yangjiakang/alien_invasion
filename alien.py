b337686fd903ae6b055f0f95554d0738c1194334
yangjiakang <18817958817@163.com>
2020-12-01 10:13:48 +0800

    检查是否撞到了屏幕的边界

--- alien.py
+++ alien.py
@@ -22,5 +22,11 @@ class Alien(Sprite):
 
     def update(self):
         """向右移动外星人"""
-        self.x += self.settings.alien_speed
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
         self.rect.x = self.x
+
+    def check_edges(self):
+        """如果外星人位于屏幕边缘，就返回True"""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
